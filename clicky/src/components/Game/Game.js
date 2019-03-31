e3cdcf45c1f8f08d34a831a327de0cb3d7e1d828
Easter Bohanon <eastdabeast@email.arizona.edu>
2019-03-30 21:26:22 -0700

    yes

---
+++ clicky/src/components/Game/Game.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Header from "../Header/header.js";
+
+class Game extends Component {
+    
+    render(){
+        return  <Header name= "ana"/>;
+    }
+}
+
+export default Game;
---
+++ clicky/src/components/Header/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AppBar from '@material-ui/core/AppBar';
+import "./header.css";
+
+
+function Header () {
+    return (
+        <AppBar className="header"> Click That SuperHero</AppBar>
+    )
+}
+
+    export default Header; 
